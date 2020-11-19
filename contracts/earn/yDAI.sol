9a31dee91ebd3dc1e495f7cafead33888b413ae8
liyu <txdev6@gmail.com>
2020-11-19 20:39:08 +0800

    add: yDAI

--- contracts/earn/yDAI.sol
+++ contracts/earn/yDAI.sol
@@ -1,11 +1,12 @@
 /**
- *Submitted for verification at Etherscan.io on 2020-02-12 v2
- https://etherscan.io/address/0x83f798e925bcd4017eb265844fddabb448f1707d#code
- 没有 Redeem 需要自己 rebalance
+ *Submitted for verification at Etherscan.io on 2020-02-12
+
+ 有 Redeem 可以用户发起 rebalance
 */
 
-pragma solidity ^0.5.16;
+pragma solidity ^0.5.0;
 pragma experimental ABIEncoderV2;
+
 
 import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
@@ -40,7 +41,7 @@ contract Structs {
     }
 
     enum ActionType {
-        Deposit, // supply tokens
+        Deposit,   // supply tokens
         Withdraw  // borrow tokens
     }
 
@@ -83,12 +84,11 @@ contract Structs {
 
 contract DyDx is Structs {
     function getAccountWei(Info memory account, uint256 marketId) public view returns (Wei memory);
-
     function operate(Info[] memory, ActionArgs[] memory) public;
 }
 
 
-contract yUSDT is ERC20, ERC20Detailed, ReentrancyGuard, Ownable, Structs {
+contract yDAI is ERC20, ERC20Detailed, ReentrancyGuard, Structs {
     using SafeERC20 for IERC20;
     using Address for address;
     using SafeMath for uint256;
@@ -113,44 +113,16 @@ contract yUSDT is ERC20, ERC20Detailed, ReentrancyGuard, Ownable, Structs {
 
     Lender public provider = Lender.NONE;
 
-    constructor () public ERC20Detailed("iearn USDT", "yUSDT", 6) {
-        //Tether USD USDT
-        token = address(0xdAC17F958D2ee523a2206206994597C13D831ec7);
-        // IEarnAPRWithPool
+    constructor () public ERC20Detailed("iearn DAI", "yDAI", 18) {
+        token = address(0x6B175474E89094C44Da98b954EedeAC495271d0F);
         apr = address(0xdD6d648C991f7d47454354f4Ef326b04025a48A8);
-        //dydx SoloMargin
         dydx = address(0x1E0447b19BB6EcFdAe1e4AE1694b0C3659614e4e);
-        //LendingPoolAddressesProvider
         aave = address(0x24a42fD28C976A61Df5D00D0599C34c4f90748c8);
-        // Fulcrum USDC iToken iUSDC
-        fulcrum = address(0xF013406A0B1d544238083DF0B93ad0d2cBE0f65f);
-        // AToken-》 aUSDT
-        aaveToken = address(0x71fc860F7D3A592A4a98740e39dB31d25db65ae8);
-        // Compound USD Coin cUSDC
-        //compound = address(0x39AA39c021dfbaE8faC545936693aC917d5E7563);
-        // Compound USDT -》cUSDT
-        compound = address(0xf650C3d88D12dB855b8bf7D11Be6C55A4e07dCC9);
-
-        dToken = 0;
-        // 市场id
-        //  approveToken();
-    }
-
-    // Ownable setters incase of support in future for these systems
-    function set_new_APR(address _new_APR) public onlyOwner {
-        apr = _new_APR;
-    }
-
-    function set_new_FULCRUM(address _new_FULCRUM) public onlyOwner {
-        fulcrum = _new_FULCRUM;
-    }
-
-    function set_new_COMPOUND(address _new_COMPOUND) public onlyOwner {
-        compound = _new_COMPOUND;
-    }
-
-    function set_new_DTOKEN(uint256 _new_DTOKEN) public onlyOwner {
-        dToken = _new_DTOKEN;
+        fulcrum = address(0x493C57C4763932315A328269E1ADaD09653B9081);
+        aaveToken = address(0xfC1E690f61EFd961294b3e1Ce3313fBD8aa4f85d);
+        compound = address(0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643);
+        dToken = 3;
+        approveToken();
     }
 
     // Quick swap low gas method for pool swaps
@@ -159,10 +131,8 @@ contract yUSDT is ERC20, ERC20Detailed, ReentrancyGuard, Ownable, Structs {
     nonReentrant
     {
         require(_amount > 0, "deposit must be greater than 0");
-        // 累加合约地址拥有的token
         pool = _calcPoolValueInToken();
 
-        // 将msg.sender的USDT的转入合约，
         IERC20(token).safeTransferFrom(msg.sender, address(this), _amount);
 
         // Calculate pool shares
@@ -171,17 +141,13 @@ contract yUSDT is ERC20, ERC20Detailed, ReentrancyGuard, Ownable, Structs {
             shares = _amount;
             pool = _amount;
         } else {
-            // （充值量* yUSDT量)/pool池子总量
             shares = (_amount.mul(_totalSupply)).div(pool);
         }
-        // 累加合约地址拥有的token，池量增加了充值
         pool = _calcPoolValueInToken();
-        //通过充值铸造 yUSDT
         _mint(msg.sender, shares);
     }
 
     // No rebalance implementation for lower fees and faster swaps
-    // 用户 按照比例体现
     function withdraw(uint256 _shares)
     external
     nonReentrant
@@ -194,35 +160,28 @@ contract yUSDT is ERC20, ERC20Detailed, ReentrancyGuard, Ownable, Structs {
         // Could have over value from cTokens
         pool = _calcPoolValueInToken();
         // Calc to redeem before updating balances
-        // 计算用户在池中的拥有的量 (pool*shares) / totalSupply
         uint256 r = (pool.mul(_shares)).div(_totalSupply);
 
-        //更新 用户的体现后余额
-        _balances[msg.sender] = _balances[msg.sender].sub(_shares, "redeem amount exceeds balance");
 
-        //在总量上减去 用户持有的股份
+        _balances[msg.sender] = _balances[msg.sender].sub(_shares, "redeem amount exceeds balance");
         _totalSupply = _totalSupply.sub(_shares);
 
         emit Transfer(msg.sender, address(0), _shares);
 
         // Check balance
-        // 获取合约地址在USDT上的余额
         uint256 b = IERC20(token).balanceOf(address(this));
         if (b < r) {
-            // r 在pool中拥有的量，b 合约地址拥有的USDT 量
-            // 通过外部函数指定 体现的池
             _withdrawSome(r.sub(b));
         }
-        // 从yUSDT 合约往发起用户体现
-        IERC20(token).safeTransfer(msg.sender, r);
+
+        IERC20(token).transfer(msg.sender, r);
         pool = _calcPoolValueInToken();
     }
 
     function() external payable {
-        //直接transfor ETH 不做处理
+
     }
 
-    // 推荐
     function recommend() public view returns (Lender) {
         (,uint256 capr,uint256 iapr,uint256 aapr,uint256 dapr) = IIEarnManager(apr).recommend(token);
         uint256 max = 0;
@@ -252,7 +211,7 @@ contract yUSDT is ERC20, ERC20Detailed, ReentrancyGuard, Ownable, Structs {
         return newProvider;
     }
 
-    function supplyDydx(uint256 amount) public {
+    function supplyDydx(uint256 amount) public   {
         Info[] memory infos = new Info[](1);
         infos[0] = Info(address(this), 0);
 
@@ -291,18 +250,15 @@ contract yUSDT is ERC20, ERC20Detailed, ReentrancyGuard, Ownable, Structs {
     function getAave() public view returns (address) {
         return LendingPoolAddressesProvider(aave).getLendingPool();
     }
-
     function getAaveCore() public view returns (address) {
         return LendingPoolAddressesProvider(aave).getLendingPoolCore();
     }
 
     function approveToken() public {
-        IERC20(token).safeApprove(compound, uint(- 1));
-        //also add to constructor
-        IERC20(token).safeApprove(dydx, uint(- 1));
-        IERC20(token).safeApprove(getAaveCore(), uint(- 1));
-        // aave 需要获取代理地址
-        IERC20(token).safeApprove(fulcrum, uint(- 1));
+        IERC20(token).safeApprove(compound, uint(-1)); //also add to constructor
+        IERC20(token).safeApprove(dydx, uint(-1));
+        IERC20(token).safeApprove(getAaveCore(), uint(-1));
+        IERC20(token).safeApprove(fulcrum, uint(-1));
     }
 
     function balance() public view returns (uint256) {
@@ -313,11 +269,9 @@ contract yUSDT is ERC20, ERC20Detailed, ReentrancyGuard, Ownable, Structs {
         Wei memory bal = DyDx(dydx).getAccountWei(Info(address(this), 0), dToken);
         return bal.value;
     }
-
     function balanceCompound() public view returns (uint256) {
         return IERC20(compound).balanceOf(address(this));
     }
-
     function balanceCompoundInToken() public view returns (uint256) {
         // Mantisa 1e18 to decimals
         uint256 b = balanceCompound();
@@ -326,7 +280,6 @@ contract yUSDT is ERC20, ERC20Detailed, ReentrancyGuard, Ownable, Structs {
         }
         return b;
     }
-
     function balanceFulcrumInToken() public view returns (uint256) {
         uint256 b = balanceFulcrum();
         if (b > 0) {
@@ -334,11 +287,9 @@ contract yUSDT is ERC20, ERC20Detailed, ReentrancyGuard, Ownable, Structs {
         }
         return b;
     }
-
     function balanceFulcrum() public view returns (uint256) {
         return IERC20(fulcrum).balanceOf(address(this));
     }
-
     function balanceAave() public view returns (uint256) {
         return IERC20(aaveToken).balanceOf(address(this));
     }
@@ -351,11 +302,9 @@ contract yUSDT is ERC20, ERC20Detailed, ReentrancyGuard, Ownable, Structs {
         Wei memory bal = DyDx(dydx).getAccountWei(Info(address(this), 0), dToken);
         return bal.value;
     }
-
     function _balanceCompound() internal view returns (uint256) {
         return IERC20(compound).balanceOf(address(this));
     }
-
     function _balanceCompoundInToken() internal view returns (uint256) {
         // Mantisa 1e18 to decimals
         uint256 b = balanceCompound();
@@ -364,7 +313,6 @@ contract yUSDT is ERC20, ERC20Detailed, ReentrancyGuard, Ownable, Structs {
         }
         return b;
     }
-
     function _balanceFulcrumInToken() internal view returns (uint256) {
         uint256 b = balanceFulcrum();
         if (b > 0) {
@@ -372,11 +320,9 @@ contract yUSDT is ERC20, ERC20Detailed, ReentrancyGuard, Ownable, Structs {
         }
         return b;
     }
-
     function _balanceFulcrum() internal view returns (uint256) {
         return IERC20(fulcrum).balanceOf(address(this));
     }
-
     function _balanceAave() internal view returns (uint256) {
         return IERC20(aaveToken).balanceOf(address(this));
     }
@@ -412,11 +358,9 @@ contract yUSDT is ERC20, ERC20Detailed, ReentrancyGuard, Ownable, Structs {
     // 1999999614570950845
     function _withdrawSomeFulcrum(uint256 _amount) internal {
         // Balance of fulcrum tokens, 1 iDAI = 1.00x DAI
-        uint256 b = balanceFulcrum();
-        // 1970469086655766652
+        uint256 b = balanceFulcrum(); // 1970469086655766652
         // Balance of token in fulcrum
-        uint256 bT = balanceFulcrumInToken();
-        // 2000000803224344406
+        uint256 bT = balanceFulcrumInToken(); // 2000000803224344406
         require(bT >= _amount, "insufficient funds");
         // can have unintentional rounding errors
         uint256 amount = (b.mul(_amount)).div(bT).add(1);
@@ -440,7 +384,6 @@ contract yUSDT is ERC20, ERC20Detailed, ReentrancyGuard, Ownable, Structs {
         }
     }
 
-    // 移仓
     function rebalance() public {
         Lender newProvider = recommend();
 
@@ -448,7 +391,6 @@ contract yUSDT is ERC20, ERC20Detailed, ReentrancyGuard, Ownable, Structs {
             _withdrawAll();
         }
 
-        // 获取 合约中USDT 的余额 移动到价格最合适的 合约中
         if (balance() > 0) {
             if (newProvider == Lender.DYDX) {
                 supplyDydx(balance());
@@ -483,25 +425,43 @@ contract yUSDT is ERC20, ERC20Detailed, ReentrancyGuard, Ownable, Structs {
     function supplyAave(uint amount) public {
         Aave(getAave()).deposit(token, amount, 0);
     }
-
     function supplyFulcrum(uint amount) public {
         require(Fulcrum(fulcrum).mint(address(this), amount) > 0, "FULCRUM: supply failed");
     }
-
     function supplyCompound(uint amount) public {
         require(Compound(compound).mint(amount) == 0, "COMPOUND: supply failed");
     }
-
     function _withdrawAave(uint amount) internal {
         AToken(aaveToken).redeem(amount);
     }
-
     function _withdrawFulcrum(uint amount) internal {
         require(Fulcrum(fulcrum).burn(address(this), amount) > 0, "FULCRUM: withdraw failed");
     }
-
     function _withdrawCompound(uint amount) internal {
         require(Compound(compound).redeem(amount) == 0, "COMPOUND: withdraw failed");
+    }
+
+    function invest(uint256 _amount)
+    external
+    nonReentrant
+    {
+        require(_amount > 0, "deposit must be greater than 0");
+        pool = calcPoolValueInToken();
+
+        IERC20(token).safeTransferFrom(msg.sender, address(this), _amount);
+
+        rebalance();
+
+        // Calculate pool shares
+        uint256 shares = 0;
+        if (pool == 0) {
+            shares = _amount;
+            pool = _amount;
+        } else {
+            shares = (_amount.mul(_totalSupply)).div(pool);
+        }
+        pool = calcPoolValueInToken();
+        _mint(msg.sender, shares);
     }
 
     function _calcPoolValueInToken() internal view returns (uint) {
@@ -523,5 +483,46 @@ contract yUSDT is ERC20, ERC20Detailed, ReentrancyGuard, Ownable, Structs {
     function getPricePerFullShare() public view returns (uint) {
         uint _pool = calcPoolValueInToken();
         return _pool.mul(1e18).div(_totalSupply);
+    }
+
+    // Redeem any invested tokens from the pool
+    function redeem(uint256 _shares)
+    external
+    nonReentrant
+    {
+        require(_shares > 0, "withdraw must be greater than 0");
+
+        uint256 ibalance = balanceOf(msg.sender);
+        require(_shares <= ibalance, "insufficient balance");
+
+        // Could have over value from cTokens
+        pool = calcPoolValueInToken();
+        // Calc to redeem before updating balances
+        uint256 r = (pool.mul(_shares)).div(_totalSupply);
+
+
+        _balances[msg.sender] = _balances[msg.sender].sub(_shares, "redeem amount exceeds balance");
+        _totalSupply = _totalSupply.sub(_shares);
+
+        emit Transfer(msg.sender, address(0), _shares);
+
+        // Check ETH balance
+        uint256 b = IERC20(token).balanceOf(address(this));
+        Lender newProvider = provider;
+        if (b < r) {
+            newProvider = recommend();
+            if (newProvider != provider) {
+                _withdrawAll();
+            } else {
+                _withdrawSome(r.sub(b));
+            }
+        }
+
+        IERC20(token).safeTransfer(msg.sender, r);
+
+        if (newProvider != provider) {
+            _rebalance(newProvider);
+        }
+        pool = calcPoolValueInToken();
     }
 }
